577c2fb175660e815915ef83bceb879145b69cbc
zhao_wei_nan <326747337@qq.com>
2025-09-17 10:29:00 +0800

    feat: update

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -1,6 +1,6 @@
 return {
   { import = "lazyvim.plugins.extras.linting.eslint" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.formatting.biome" },
   { import = "lazyvim.plugins.extras.lsp.neoconf" },
   { import = "lazyvim.plugins.extras.editor.refactoring" },
   { import = "lazyvim.plugins.extras.editor.overseer" },
